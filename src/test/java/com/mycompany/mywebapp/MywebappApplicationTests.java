1a4557947b4fa58c2d239b726f71ddbbed5d7e6f
KelliKirk <kelli.kirk@voco.ee>
2026-03-19 11:18:03 +0200

    Test user creation and show user list

--- src/test/java/com/mycompany/mywebapp/MywebappApplicationTests.java
+++ src/test/java/com/mycompany/mywebapp/MywebappApplicationTests.java
@@ -1,13 +1,46 @@
 package com.mycompany.mywebapp;
 
+import com.mycompany.mywebapp.user.User;
+import com.mycompany.mywebapp.user.UserRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 
-@SpringBootTest
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(value = false)
 class MywebappApplicationTests {
 
+    @Autowired
+    private UserRepository repo;
     @Test
-    void contextLoads() {
+    void testAddUser() {
+        User user = new User();
+        user.setFirstName("Zooja");
+        user.setLastName("Puhur");
+        user.setEmail("zooja.puhur@voco.ee");
+        user.setPassword("password123");
+
+        User savedUser = repo.save(user);
+        System.out.println(savedUser);
+
+        Assertions.assertThat(savedUser).isNotNull();
+        Assertions.assertThat(savedUser.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    void testListAll() {
+        Iterable<User> users = repo.findAll();
+        Assertions.assertThat(users).hasSizeGreaterThan(0);
+
+        for (User user: users){
+            System.out.println(user);
+        }
     }
 
 }
+
