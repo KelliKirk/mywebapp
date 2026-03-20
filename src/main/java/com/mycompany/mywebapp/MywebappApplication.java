7a4a71407e8add7a557a5bbf91b31b954752432d
KelliKirk <kelli.kirk@voco.ee>
2026-03-20 09:12:58 +0200

    Create Tomcat deployment server

--- src/main/java/com/mycompany/mywebapp/MywebappApplication.java
+++ src/main/java/com/mycompany/mywebapp/MywebappApplication.java
@@ -2,12 +2,20 @@ package com.mycompany.mywebapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MywebappApplication {
+public class MywebappApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
+
         SpringApplication.run(MywebappApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MywebappApplication.class);
     }
 
 }
